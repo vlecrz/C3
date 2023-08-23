2797586a1abd264bfd4394c5b9745b55ad71d13c
Valentina Cruz <141838196+vlecrz@users.noreply.github.com>
2023-08-23 08:18:44 -0400

    Add files via upload

---
+++ json cursos.js
@@ -0,0 +1,26 @@
+[{
+        "name": "Lengua Latina I",
+        "code": "LET0133",
+        "about": "Me interesan mucho las lenguas muertas y en especial el latín del cual proviene nuestro idioma el español."
+    },
+    {
+        "name": "Introducción a la Lengua y Literatura Sánscritas",
+        "code": "LET103E",
+        "about": "Este ramo lo estoy actualmente cursando y es por mucho uno de los más interesantes que he tomado. Aparte de enseñar la lengua que de por sí es interesantísima tanto por lo oral como por lo escrito, nos muestra el lado histórico cultural del lenguaje, adentrándonos en un mundo que de otra manera sería muy difícil adentrarse."
+    },
+    {
+        "name": "Lengua y Cultura China",
+        "code": "LET052E",
+        "about": "Como se podrá ver de los dos anteriores, me interesan muchos los idiomas. Este ramo en específico lo encuentro interesante por la manera de abordar la adquisición del lenguaje del infante y de sus procesos."
+    },
+    {
+        "name": "Theory & Practice of Translation",
+        "code": "LET0312",
+        "about": "El tema de la traducción de textos la encuentro fascinante. El tomar las palabras de alguien y pasarlas a tu idioma sin que se pierda la chispa, la esencia, siento que es un arte."
+    },
+    {
+        "name": "Strange Tales in Narrative and Film",
+        "code": "LET018I",
+        "about": "Lo tomaría sólo porque suena interesante y me tincó, además por el enfoque interdisciplinario que tiene."
+    }
+]
